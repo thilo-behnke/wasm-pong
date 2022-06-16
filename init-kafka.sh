9106f6c2a32241a4b6aeac6c8a3f7cba9b6323ae
Thilo Behnke <thilo.behnke@gmx.net>
2022-06-16 16:18:32 +0200

    dev-ops/fix-wasm-client

---
+++ init-kafka.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -e
+
+docker exec pong_server_kafka /opt/bitnami/kafka/bin/kafka-topics.sh --create --topic session --bootstrap-server "$KAFKA_HOST:$KAFKA_PORT"
+docker exec pong_server_kafka /opt/bitnami/kafka/bin/kafka-topics.sh --create --topic move --bootstrap-server "$KAFKA_HOST:$KAFKA_PORT"
+docker exec pong_server_kafka /opt/bitnami/kafka/bin/kafka-topics.sh --create --topic status --bootstrap-server "$KAFKA_HOST:$KAFKA_PORT"
+docker exec pong_server_kafka /opt/bitnami/kafka/bin/kafka-topics.sh --create --topic input --bootstrap-server "$KAFKA_HOST:$KAFKA_PORT"

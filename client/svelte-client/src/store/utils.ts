882c83b589f406517cf75305f6aecae130c5df81
Thilo Behnke <thilo.behnke@gmx.net>
2022-06-28 23:13:53 +0200

    combined store

--- client/svelte-client/src/store/utils.ts
+++ client/svelte-client/src/store/utils.ts
@@ -1,7 +1,36 @@
-import {readable} from "svelte/store";
+import {get, Readable, readable, Unsubscriber, writable} from "svelte/store";
+import type {Subscriber} from "svelte/types/runtime/store";
 
 export const timer = ms => readable(ms, function(set) {
     setTimeout(() => {
         set(0)
     }, ms)
 })
+
+export function combined<T>(store1: Readable<T[]>, store2: Readable<T[]>): Readable<T[]> {
+    const val1 = writable<T[]>([]);
+    const val2 = writable<T[]>([]);
+
+    const {set, subscribe} = writable<T[]>();
+
+    const unsub1 = store1.subscribe(val => {
+        val1.set(val);
+        set([...val, ...get(val2)]);
+    })
+    const unsub2 = store2.subscribe(val => {
+        val2.set(val);
+        set([...get(val1), ...val]);
+    })
+
+    const customSubscribe = (run: Subscriber<T[]>, invalidate): Unsubscriber => {
+        const unsub = subscribe(run, invalidate);
+        return () => {
+            unsub();
+            unsub1();
+            unsub2();
+        }
+    }
+    return {
+        subscribe: customSubscribe
+    }
+}
